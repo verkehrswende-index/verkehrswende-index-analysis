9a6e3e214995a14e37039f12debd2aff59abf948
Christian Neumann <cneumann@e3code.de>
2021-06-21 16:23:57 +0200

    Add more locations.

--- src/commands/fetch-locations.mjs
+++ src/commands/fetch-locations.mjs
@@ -1,5 +1,8 @@
 import {DOMParser} from "xmldom";
 
+/**
+ * Retrieves all areas to consider (cities) from the main extracts.
+ */
 export default class FetchLocations {
   constructor(osmium, store, nameToSlug) {
     this.osmium = osmium;
@@ -17,30 +20,33 @@ export default class FetchLocations {
       timeSpan = '1y';
     }
     var cities = null;
-    var cities = await this.osmium.exec(`tags-filter -f osm -R ${this.getDataPath(argv)} r/de:place=city`.split(' ')) ;
+    var cities = await this.osmium.exec(`tags-filter -f osm -R ${this.getDataPath(argv)} r/de:place=city,town`.split(' ')) ;
     var internationals = await this.osmium.exec(`getid -f osm ${this.getDataPath(argv,'denmark')} r2192363`.split(' ')) ; // Kopenhagen
     internationals = (new DOMParser()).parseFromString(internationals, 'text/xml');
     cities = (new DOMParser()).parseFromString(cities, 'text/xml');
     var areas = [];
+    var seenAreas = {};
     const cityRelations = cities.documentElement.getElementsByTagName('relation');
     const internationalsRelations = internationals.documentElement.getElementsByTagName('relation');
     for(const relations of [cityRelations, internationalsRelations]) {
-      relations:
       for(let i = 0; i < relations.length; i++) {
         let area = {
-          extract: 'denmark',
+          extract: relations === internationalsRelations ? 'denmark' : null,
           id: relations[i].getAttribute('id'),
         };
         const tags = relations[i].getElementsByTagName('tag');
         for(let i = 0; i < tags.length; i++) {
           area[tags[i].getAttribute('k')] = tags[i].getAttribute('v');
-          // if(tags[i].getAttribute('k') === 'name') {
-          //   area.slug = this.nameToSlug.getSlug(area.name);
-          // }
+        }
+        const slug = this.nameToSlug.getSlug(area.name);
+        if(slug in seenAreas) {
+          console.log("Duplicate area slug, ignoring:", slug);
+          continue;
         }
         areas.push(area);
+        seenAreas[slug] = true;
       };
     }
-    this.store.write(`areas.json`, areas);
+    await this.store.write(`areas.json`, areas);
   };
 }
